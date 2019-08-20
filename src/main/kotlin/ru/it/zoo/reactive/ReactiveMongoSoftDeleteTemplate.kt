68954ffd80492611a5a0eb510a07ce7b75a3e423
kostya05983 <kostya05983@mail.ru>
2019-08-21 01:48:51 +0700

    [UPD] add logic with update

--- src/main/kotlin/ru/it/zoo/reactive/ReactiveMongoSoftDeleteTemplate.kt
+++ src/main/kotlin/ru/it/zoo/reactive/ReactiveMongoSoftDeleteTemplate.kt
@@ -1,10 +1,16 @@
 package ru.it.zoo.reactive
 
+import com.mongodb.WriteConcern
+import com.mongodb.client.model.UpdateOptions
 import com.mongodb.client.result.DeleteResult
 import com.mongodb.lang.Nullable
+import com.mongodb.reactivestreams.client.MongoCollection
+import org.bson.Document
 import org.springframework.dao.InvalidDataAccessApiUsageException
 import org.springframework.data.mapping.context.MappingContext
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.core.MongoAction
+import org.springframework.data.mongodb.core.MongoActionOperation
 import org.springframework.data.mongodb.core.ReactiveCollectionCallback
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.convert.MongoConverter
@@ -25,12 +31,20 @@ class ReactiveMongoSoftDeleteTemplate(
 
     private val mappingContext: MappingContext<out MongoPersistentEntity<*>, MongoPersistentProperty>
 
+    private var writeConcernOverride: WriteConcern? = null
+
+    override fun setWriteConcern(writeConcern: WriteConcern?) {
+        writeConcernOverride = writeConcern
+        super.setWriteConcern(writeConcern)
+    }
+
     init {
         mappingContext = mongoConverter.mappingContext
     }
 
     private companion object {
     }
+
 
     override fun <T : Any?> doRemove(
         collectionName: String,
@@ -47,12 +61,41 @@ class ReactiveMongoSoftDeleteTemplate(
         val queryObject = query.queryObject
         val entity = getPersistentEntity(entityClass)
 
-        execute(collectionName, ReactiveCollectionCallback<T> {
+        execute(collectionName, ReactiveCollectionCallback<T> { collection ->
             TODO("Update logic instead of remove")
+            val updateObj = Document() // TODO("Change to update with deleted field")
+            val mongoAction = MongoAction(
+                writeConcernOverride, MongoActionOperation.UPDATE, collectionName, entityClass,
+                updateObj, queryObject
+            )
+
+            val writeConcernToUse = prepareWriteConcern(mongoAction)
+            val collectionToUse = prepareCollection(collection, writeConcernToUse)
+
+            val updateOptions = UpdateOptions()
+            query.collation.map { it.toMongoCollation() }.ifPresent {
+                updateOptions.collation = it
+            }
+
+            collectionToUse.updateMany(queryObject, updateObj, updateOptions)
+            //TODO use adapter to convert updateResult to DelteResult
+
         }).doOnNext {
             //            maybeEmitEvent(AfterDeleteEvent<>())
         }.next()
         return super.doRemove(collectionName, query, entityClass)
+    }
+
+    private fun prepareCollection(
+        collection: MongoCollection<Document>,
+        writeConcernToUse: WriteConcern?
+    ): MongoCollection<Document> {
+        var collectionToUse = collection
+
+        if (writeConcernToUse != null) {
+            collectionToUse = collectionToUse.withWriteConcern(writeConcernToUse)
+        }
+        return collectionToUse
     }
 
     private fun getPersistentEntity(type: Class<*>?): MongoPersistentEntity<*>? {
