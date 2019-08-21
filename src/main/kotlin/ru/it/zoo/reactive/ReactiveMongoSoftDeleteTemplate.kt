31cb37beea45d688e0fcd26f01600d0099a399c3
kostya05983 <kostya05983@mail.ru>
2019-08-21 23:58:33 +0700

    [ADD] converter from updateResult to DeleteResult, override doRemove

--- src/main/kotlin/ru/it/zoo/reactive/ReactiveMongoSoftDeleteTemplate.kt
+++ src/main/kotlin/ru/it/zoo/reactive/ReactiveMongoSoftDeleteTemplate.kt
@@ -3,7 +3,6 @@ package ru.it.zoo.reactive
 import com.mongodb.WriteConcern
 import com.mongodb.client.model.UpdateOptions
 import com.mongodb.client.result.DeleteResult
-import com.mongodb.lang.Nullable
 import com.mongodb.reactivestreams.client.MongoCollection
 import org.bson.Document
 import org.springframework.dao.InvalidDataAccessApiUsageException
@@ -11,13 +10,15 @@ import org.springframework.data.mapping.context.MappingContext
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
 import org.springframework.data.mongodb.core.MongoAction
 import org.springframework.data.mongodb.core.MongoActionOperation
-import org.springframework.data.mongodb.core.ReactiveCollectionCallback
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.convert.MongoConverter
+import org.springframework.data.mongodb.core.convert.QueryMapper
+import org.springframework.data.mongodb.core.convert.UpdateMapper
 import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity
 import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty
-import org.springframework.data.mongodb.core.mapping.event.AfterDeleteEvent
+import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
 import org.springframework.util.Assert
 import reactor.core.publisher.Mono
 
@@ -28,30 +29,33 @@ class ReactiveMongoSoftDeleteTemplate(
     mongoDatabaseFactory: ReactiveMongoDatabaseFactory,
     mongoConverter: MongoConverter
 ) : ReactiveMongoTemplate(mongoDatabaseFactory, mongoConverter) {
-
-    private val mappingContext: MappingContext<out MongoPersistentEntity<*>, MongoPersistentProperty>
-
+    private val mappingContext: MappingContext<out MongoPersistentEntity<*>, MongoPersistentProperty> =
+        mongoConverter.mappingContext
+    private val queryMapper: QueryMapper = QueryMapper(mongoConverter)
+    private val updateMapper: UpdateMapper = UpdateMapper(mongoConverter)
     private var writeConcernOverride: WriteConcern? = null
+
+    private companion object {
+        private val REMOVE_CRITERIA = Criteria().orOperator(
+            Criteria.where("deleted").exists(false),
+            Criteria.where("deleted").`is`(false)
+        )
+
+        private val UPDATE_DEL = Update().apply {
+            set("deleted", true)
+        }
+    }
 
     override fun setWriteConcern(writeConcern: WriteConcern?) {
         writeConcernOverride = writeConcern
         super.setWriteConcern(writeConcern)
     }
 
-    init {
-        mappingContext = mongoConverter.mappingContext
-    }
-
-    private companion object {
-    }
-
-
     override fun <T : Any?> doRemove(
         collectionName: String,
         query: Query?,
         entityClass: Class<T>?
     ): Mono<DeleteResult> {
-        //Reuse removeCollectionCallback
         if (query == null) {
             throw InvalidDataAccessApiUsageException("Query passed in to remove can't be null!")
         }
@@ -61,29 +65,30 @@ class ReactiveMongoSoftDeleteTemplate(
         val queryObject = query.queryObject
         val entity = getPersistentEntity(entityClass)
 
-        execute(collectionName, ReactiveCollectionCallback<T> { collection ->
-            TODO("Update logic instead of remove")
-            val updateObj = Document() // TODO("Change to update with deleted field")
+        val updateToDeleteResultConverter = UpdateToDeleteResultConverter()
+        return execute(collectionName) { collection ->
+            val updateQuery = queryMapper.getMappedObject(queryObject, entity)
+            updateQuery.merge(REMOVE_CRITERIA.criteriaObject)
+
+            val updateObj = updateMapper.getMappedObject(UPDATE_DEL.updateObject, entity)
             val mongoAction = MongoAction(
                 writeConcernOverride, MongoActionOperation.UPDATE, collectionName, entityClass,
-                updateObj, queryObject
+                updateObj, updateQuery
             )
 
             val writeConcernToUse = prepareWriteConcern(mongoAction)
             val collectionToUse = prepareCollection(collection, writeConcernToUse)
 
+
             val updateOptions = UpdateOptions()
             query.collation.map { it.toMongoCollation() }.ifPresent {
-                updateOptions.collation = it
+                updateOptions.collation(it)
             }
 
             collectionToUse.updateMany(queryObject, updateObj, updateOptions)
-            //TODO use adapter to convert updateResult to DelteResult
-
-        }).doOnNext {
-            //            maybeEmitEvent(AfterDeleteEvent<>())
+        }.map {
+            updateToDeleteResultConverter.convert(it)
         }.next()
-        return super.doRemove(collectionName, query, entityClass)
     }
 
     private fun prepareCollection(
@@ -100,5 +105,11 @@ class ReactiveMongoSoftDeleteTemplate(
 
     private fun getPersistentEntity(type: Class<*>?): MongoPersistentEntity<*>? {
         return if (type == null) null else mappingContext.getPersistentEntity(type)
+    }
+
+    private fun Document.merge(`in`: Document) {
+        for ((key, value) in `in`) {
+            put(key, value)
+        }
     }
 }
---
+++ src/main/kotlin/ru/it/zoo/reactive/UpdateToDeleteResultConverter.kt
@@ -0,0 +1,21 @@
+package ru.it.zoo.reactive
+
+import com.mongodb.client.result.DeleteResult
+import com.mongodb.client.result.UpdateResult
+
+/**
+ * @author kostya05983
+ */
+class UpdateToDeleteResultConverter {
+
+    /**
+     * Convert oneResult to other for softDelete
+     */
+    fun convert(updateResult: UpdateResult): DeleteResult {
+        return if (updateResult.wasAcknowledged()) {
+            DeleteResult.acknowledged(updateResult.modifiedCount)
+        } else {
+            DeleteResult.unacknowledged()
+        }
+    }
+}
